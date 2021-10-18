42d8926d00b1e5304e0ce1109756daaa44c32a25
Javier <test@mail.com>
2021-10-17 23:53:19 -0300

    fix: adding CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import usuariosRouter from './router/usuarios.router.js';
 import tipoPedidosRouter from './router/tipoPedidos.router.js';
 import estadoPedidosRouter from './router/estadoPedidos.router.js';
@@ -7,7 +8,21 @@ import matafuegosRouter from './router/matafuegos.router.js';
 import localesRouter from './router/locales.router.js';
 import loginRouter from './router/login.router.js';
 
+const whiteList = ['http://localhost:8080']; 
+
+const corsOptions = {
+    origin: function(origin, callback) {
+        if (whiteList.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}
+
+
 const app = express()
+app.use(cors())
 
 // Parse Body JSON
 app.use(express.json())
